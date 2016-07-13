3d4e5c4de0848583c881346cd69fbd4796e05a73
Will Charczuk <will.charczuk@gmail.com>
2016-07-13 15:48:30 -0700

    collections??

---
+++ vendor/github.com/blendlabs/go-util/collections/queue.go
@@ -0,0 +1,14 @@
+package collections
+
+// Queue is an interface for implementations of a FIFO buffer.
+type Queue interface {
+	Len() int
+	Enqueue(value interface{})
+	Dequeue() interface{}
+	Peek() interface{}
+	PeekBack() interface{}
+	AsSlice() []interface{}
+	Clear()
+
+	Each(consumer func(value interface{}))
+}
